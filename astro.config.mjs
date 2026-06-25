af4509f9caf418abedfabdd56d2d141a010bf2bd
tp4423 <xinkj521@gmail.com>
2026-06-25 08:09:56 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -56,7 +56,7 @@ const adapter = process.env.CF_WORKERS
 export default defineConfig({
 	site: siteConfig.site_url,
 
-	base: "/",
+	base: "/Astro/",
 	trailingSlash: "always",
 
 	// 字体配置 - 只加载实际使用的字体，跳过未引用的以加快构建
